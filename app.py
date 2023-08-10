3ec09b0acd6a521affac985dcd53bf8154630615
nickhillease <86102977+nickhillease@users.noreply.github.com>
2023-08-10 23:38:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import joblib
+#loading the model
+model_filename='decTree.joblib'
+saved_model=joblib.load(model_filename)
+
+
+st.title('Iris Flowers Classification ')
+sepal_length = st.number_input('Enter Sepal Length:')
+sepal_width = st.number_input('Enter Sepal Width:')
+petal_length = st.number_input('Enter Petal Length:')
+petal_width = st.number_input('Enter Petal Width:')
+
+# Sepal length,Sepal Width,Petal Length,Petal Width
+if st.button('Predict'):
+     features=[[sepal_length, sepal_width, petal_length, petal_width]]
+     prediction=saved_model.predict(features)
+
+     if prediction==0:
+          st.write('The given flower is Iris Setosa')
+     elif prediction==1:
+          st.write("The given flower is Iris Versicolor")
+     elif prediction==2:
+          st.write('The given flower is Iris Virginica')
+     else:
+          st.write('Invalid Prediction')
+     
+     
